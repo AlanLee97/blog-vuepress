c6c18d74a2213ae7efae7298a6afd3702abf815b
1445654576@qq.com <1445654576@qq.com>
2021-10-03 20:08:45 +0800

    fix: 删除dist目录

--- dist/assets/js/301.0e06cea7.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[301],{738:function(n,w,o){}}]);
--- dist/assets/js/304.4f715a91.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[304],{741:function(n,w,o){}}]);
